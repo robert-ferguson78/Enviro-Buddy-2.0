81dcc963e25a3ff49bd95fc37143cf89d8eaa557
Robert Ferguson <robert.ferguson@digitalstrategy.ie>
2024-04-19 18:47:28 +0100

    js to ts files converted

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,11 +1,11 @@
-// place files you want to import through the `$lib` alias in this folder.
-export { default as serviceAccount } from './firebase/firebase-secrets.server.json';
+// // place files you want to import through the `$lib` alias in this folder.
+// export { default as serviceAccount } from './firebase/firebase-secrets.server.json';
 
-// Re-exporting modules from the 'firebase' directory
-export { loginWithGoogle } from './firebase/auth.client';
+// // Re-exporting modules from the 'firebase' directory
+// export { loginWithGoogle } from './firebase/auth.client';
 
-// Re-exporting modules from the 'helpers' directory
-export { afterLogin } from './helpers/route.helper';
+// // Re-exporting modules from the 'helpers' directory
+// export { afterLogin } from './helpers/route.helper';
 
-// Re-exporting modules from the 'stores' directory
-export { default as messagesStore } from './stores/messages.store';
+// // Re-exporting modules from the 'stores' directory
+// export { default as messagesStore } from './stores/messages.store';
