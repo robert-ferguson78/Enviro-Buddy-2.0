8afaca933d2402e685f2af53688b6e56ad53faa9
Robert Ferguson <20104121@mail.wit.ie>
2025-01-12 16:37:42 +0000

    adjusted vite.config for performance and development conflicts

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,16 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	optimizeDeps: { 
+		include: ['@sveltejs/kit'] // Include the SvelteKit package in the optimized dependencies
+	},
+	resolve: { 
+		dedupe: ['@sveltejs/kit'] // Dedupe the SvelteKit package - prevents it from being included multiple times
+	},
+	server: {
+		fs: {
+			strict: false // Allow reading files from the file system
+		}
+	}
 });
